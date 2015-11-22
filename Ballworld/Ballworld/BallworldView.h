9aafdeb003bc117bca3828161b02efc583a52b64
nkmrh <hajimeet@me.com>
2015-11-22 22:06:58 +0900

    Init

---
+++ Ballworld/Ballworld/BallworldView.h
@@ -0,0 +1,13 @@
+//
+//  BallworldView.h
+//  Ballworld
+//
+//  Created by Nakamura Hajime on 11/22/15.
+//
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BallworldView : ScreenSaverView
+
+@end
