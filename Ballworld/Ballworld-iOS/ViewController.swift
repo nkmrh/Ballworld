844748b22870777360847a65a7f2d9a718459bc3
nkmrh <hajimeet@me.com>
2015-11-23 14:24:12 +0900

    Show a triangle

--- Ballworld/Ballworld-iOS/ViewController.swift
+++ Ballworld/Ballworld-iOS/ViewController.swift
@@ -1,17 +1,86 @@
 import UIKit
+import Metal
+import QuartzCore
 
 class ViewController: UIViewController {
+    
+    var device: MTLDevice! = nil
+    var metalLayer: CAMetalLayer! = nil
+    
+    let vertexData:[Float] = [
+        0.0, 1.0, 0.0,
+        -1.0, -1.0, 0.0,
+        1.0, -1.0, 0.0]
+    
+    var vertexBuffer: MTLBuffer! = nil
+    
+    var pipelineState: MTLRenderPipelineState! = nil
+    var commandQueue: MTLCommandQueue! = nil
+    var timer: CADisplayLink! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        device = MTLCreateSystemDefaultDevice()
+        
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .BGRA8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.frame
+        view.layer.addSublayer(metalLayer)
+        
+        let dataSize = vertexData.count * sizeofValue(vertexData[0])
+        vertexBuffer = device.newBufferWithBytes(vertexData, length: dataSize, options: MTLResourceOptions.CPUCacheModeDefaultCache)
+        
+        let defaultLibrary = device.newDefaultLibrary()
+        let vertexProgram = defaultLibrary!.newFunctionWithName("basic_vertex")
+        let fragmentProgram = defaultLibrary!.newFunctionWithName("basic_fragment")
+        
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .BGRA8Unorm
+        
+        do {
+            try pipelineState = device.newRenderPipelineStateWithDescriptor(pipelineStateDescriptor)
+        } catch {
+            print("An error occurred \(error)")
+            assert(false);
+        }
+        
+        commandQueue = device.newCommandQueue()
+        
+        timer = CADisplayLink(target: self, selector: Selector("loop"))
+        timer.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func render() {
+        let drawable = metalLayer.nextDrawable()
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable!.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .Clear
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
+        
+        let commandBuffer = commandQueue.commandBuffer()
+        
+        let renderEncoderOpt = commandBuffer.renderCommandEncoderWithDescriptor(renderPassDescriptor)
+        renderEncoderOpt.setRenderPipelineState(pipelineState)
+        renderEncoderOpt.setVertexBuffer(vertexBuffer, offset: 0, atIndex: 0)
+        renderEncoderOpt.drawPrimitives(.Triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+        renderEncoderOpt.endEncoding()
+        
+        commandBuffer.presentDrawable(drawable!)
+        commandBuffer.commit()
+    }
+    
+    func loop() {
+        autoreleasepool { () -> () in
+            self.render()
+        }
+    }
 }
-
